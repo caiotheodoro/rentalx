b674896293e19b76637224f0927826ad42e7ab45
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-27 23:51:55 -0300

    feat: 🎸 List categories + repository categories methods

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,26 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 const categoriesRoutes = Router();
 
 const categories: Category[] = [];
-
+const categoriesRepository = new CategoriesRepository();
 
 
 categoriesRoutes.post("/", (req,res) => {
     const { name, description } = req.body;
-
-    const category = new Category();
-
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    });
-   
-    
+    categoriesRepository.create({name, description});
    
 
-    categories.push(category);
-
-    return res.status(201).json({ category });
+    return res.status(201).send();
 })
+
+categoriesRoutes.get("/", (req,res) => {
+    const all = categoriesRepository.list();
+
+    return res.status(200).json(all);
+})
+
 
 export { categoriesRoutes }
