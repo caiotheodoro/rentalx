223d809244d030a94be5dd8acca05114243b36fa
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-02 22:35:04 -0300

    docs: ✏️ Swagger ui added and category documentation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { router } from "./routes";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(router)
 
 
