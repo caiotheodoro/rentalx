87198a35314832ecefe2d366ad9d8ab6cbb74e8c
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-02 22:10:45 -0300

    feat: 🎸 Inserting uploaded cvs into data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,19 +1,52 @@
 import fs from 'fs';
 import {parse} from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
+    constructor(private categoriesRepository: ICategoriesRepository){}
 
-        const parseFile = parse()
-
-        stream.pipe(parseFile);
-        parseFile.on('data', async (line) => {
-            console.log(line)
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = []
+            const parseFile = parse()
+    
+            stream.pipe(parseFile);
+            parseFile.on('data', async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                })
+            })
+            .on('end', () => {
+                resolve(categories);
+            })
+            .on('error', (err) => {
+                reject(err);
+            })
         })
+    }
 
-
-        console.log(file);
+    async execute(file: Express.Multer.File): Promise<void> {
+        
+        const categories = await this.loadCategories(file)
+        categories.map(async (category) => {
+            const {name, description} = category;
+            const categoryExists = this.categoriesRepository.findByName(name);
+            if(!categoryExists){
+                this.categoriesRepository.create({
+                    name,
+                    description
+                })
+            }
+            
+        })
     }
 
 }
