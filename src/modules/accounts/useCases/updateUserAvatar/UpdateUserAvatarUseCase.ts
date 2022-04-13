c3773e22d1f373f623686677741b245e03f3b7b3
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-12 23:03:26 -0300

    refactor: 💡 Delefe file if duplicated whe upload file

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { UsersRepository } from "../../repositories/implementations/UsersRepository";
-
+import { deleteFile } from "../../../../utils/file";
 interface IRequest {
     user_id: string;
     avatar_file: string;
@@ -17,6 +17,9 @@ class UpdateUserAvatarUseCase {
 
         const user = await this.usersRepository.findById(user_id);
 
+        if( user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
